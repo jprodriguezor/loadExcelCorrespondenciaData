a5e412e80052a27e8b104a4f1e38ffe11190da1c
Jorge <jorge.softdevelop@gmail.com>
2017-10-09 11:17:44 -0400

    MOD: ajuste en la URL del servicio y comentarios a nivel de metodos. La URL del servicio será http://IP:PUERTO/Massive-Loader/massiveloaderapi/{contexto_de_la_operacion}

--- Massive-Loader-Root/Web-Modules/Massive-Loader/src/main/java/co/com/soaint/correspondencia/massiveloader/web/rest/MassiveLoaderWebApi.java
+++ Massive-Loader-Root/Web-Modules/Massive-Loader/src/main/java/co/com/soaint/correspondencia/massiveloader/web/rest/MassiveLoaderWebApi.java
@@ -1,7 +1,9 @@
 package co.com.soaint.correspondencia.massiveloader.web.rest;
 
 import co.com.soaint.correspondencia.massiveloader.web.domain.DocumentVO;
+import co.com.soaint.correspondencia.massiveloader.web.infrastructure.common.ListadoCargasMasivasDTO;
 import co.com.soaint.correspondencia.massiveloader.web.infrastructure.common.MasiveLoaderResponse;
+import co.com.soaint.correspondencia.massiveloader.web.infrastructure.common.StatusMassiveLoaderProcessResponseDTO;
 import co.com.soaint.correspondencia.massiveloader.web.infrastructure.massiveloader.MassiveLoaderController;
 import co.com.soaint.foundation.documentmanager.business.comunicacionoficial.interfaces.ComOficialMgtProxy;
 import co.com.soaint.foundation.documentmanager.domain.ComunicacionOficialContainerDTO;
@@ -10,17 +12,15 @@ import co.com.soaint.foundation.documentmanager.infrastructure.massiveloader.Mas
 import co.com.soaint.foundation.documentmanager.infrastructure.massiveloader.domain.MassiveRecordContext;
 import co.com.soaint.foundation.documentmanager.infrastructure.massiveloader.executor.LoaderExecutor;
 import co.com.soaint.foundation.infrastructure.transformer.Transformer;
-import co.com.soaint.correspondencia.massiveloader.web.infrastructure.common.ListadoCargasMasivasDTO;
-import co.com.soaint.correspondencia.massiveloader.web.infrastructure.common.StatusMassiveLoaderProcessResponseDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@Controller
+@RestController
+@RequestMapping("/massiveloaderapi")
 @Log4j2
 public class MassiveLoaderWebApi extends MassiveLoaderController<DocumentVO, MassiveRecordContext <ComunicacionOficialContainerDTO>> {
 
@@ -42,8 +42,13 @@ public class MassiveLoaderWebApi extends MassiveLoaderController<DocumentVO, Mas
     }
 
 
-    //[upload service] ------------------------------
-    @ResponseBody
+    /**
+     * Servicio para la carga masiva del excel
+     * @param file contenido del excel
+     * @param codigoSede codigo de la sede que se recibe desde UI
+     * @param codigoDependencia codigo de la dependencia que se recibe desde UI
+     * @return Retorna una respuesta que contiene el estado de la operacion
+     */
     @CrossOrigin
     @RequestMapping(value = "/cargar-fichero", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     public MasiveLoaderResponse cargarFichero(@RequestParam("file") MultipartFile file, @RequestParam("codigoSede") String codigoSede, @RequestParam("codigoDependencia") String codigoDependencia) {
@@ -57,21 +62,31 @@ public class MassiveLoaderWebApi extends MassiveLoaderController<DocumentVO, Mas
                 massiveRecordTransformer, ccBuilder.build ( ), codigoSede, codigoDependencia);
     }
 
-    @ResponseBody
+    /**
+     * Operacion para obtener el estado de la ultima carga masiva
+     * @return retorna un mensaje con el estado de la ultima carga masiva
+     */
     @CrossOrigin
     @RequestMapping(value = "/estadocargamasiva", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public StatusMassiveLoaderProcessResponseDTO obtenerEstadoCargaMasiva() {
         return obtenerDataEstadoCargaMasiva ( );
     }
 
-    @ResponseBody
+    /**
+     * Operacion para obtener el estado de una operacion de carga masiva
+     * @param idCarga id de la carga masiva de la que se quiere ver su estado y contenido
+     * @return
+     */
     @CrossOrigin
     @RequestMapping(value = "/estadocargamasiva/{idCarga}", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public StatusMassiveLoaderProcessResponseDTO obtenerEstadoCargaMasivabyID(@PathVariable int idCarga) {
         return obtenerDataEstadoCargaMasivabyID (idCarga);
     }
 
-    @ResponseBody
+    /**
+     * Operacion que lista las cargas masivas
+     * @return retorna un objeto con el listado de las cargas masivas realizadas
+     */
     @CrossOrigin
     @RequestMapping(value = "/listadocargamasiva", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public ListadoCargasMasivasDTO obtenerlistadoCargaMasiva() {
